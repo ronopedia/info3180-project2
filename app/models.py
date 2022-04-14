dd4da34dd6867c8ec9559ebee0655e4c3319d79f
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-04-14 17:13:03 -0500

    Updated

--- app/models.py
+++ app/models.py
@@ -1,48 +1,6 @@
 from . import db
 from werkzeug.security import generate_password_hash
 
-
-class Users(db.Model):
-  __tablename__ = 'users'
-  
-  id = db.Column(db.Integer, primary_key=True, autoincrement = True)
-  username = db.Column(db.String(100), unique=True, nullable=False)
-  password = db.Column(db.String(255), nullable=False)
-  name = db.Column(db.String(100), nullable=False)
-  email = db.Column(db.String(100), unique=True, nullable=False)
-  location = db.Column(db.String(100), nullable=False)
-  biography = db.Column(db.String(100))
-  photo = db.Column(db.String(50), nullable=False)
-  date_joined = db.Column(db.DateTime, nullable=False, default=datetime.now)
-  
-  def __init__(self, username, password, name, email, location, biography, photo, date_joined):
-    self.username = username
-    self. password = generate_password_hash(password, method='pbkdf2:sha256')
-    self.name = name
-    self. email = email
-    self.location = location
-    self.biography = biography
-    self.photo = photo
-    self.date_joined = datetime.now()
-    
-  def __repr__(self):
-    return '<User %r>' % (self.username)  
-  
-
-class Favourites(db.Model):
-  __tablename__ = 'favourites'
-  
-  id = db.Column(db.Integer, primary_key=True, autoincrement = True)
-  car_id = db.Column(db.Integer, db.ForeignKey('cars.id'), nullable=False)
-  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  
-  def __init__(self, car_id, user_id):
-    self.car_id = car_id
-    self.user_id = user_id
-  
-  def __repr__(self):
-    return '<Favourite %r>' % (self.car_id) 
-        
 class Cars(db.Model):
   __tablename__ = 'cars'
   
@@ -70,17 +28,61 @@ class Cars(db.Model):
     self.photo = photo
     self.user_id = user_id
     
+    
+    
+class Favourites(db.Model):
+  __tablename__ = 'favourites'
+  
+  id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+  car_id = db.Column(db.Integer, db.ForeignKey('cars.id'), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  
+  def __init__(self, car_id, user_id):
+    self.car_id = car_id
+    self.user_id = user_id 
+    
+    
+    
+class Users(db.Model):
+  __tablename__ = 'users'
+  
+  id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+  username = db.Column(db.String(100), unique=True, nullable=False)
+  password = db.Column(db.String(255), nullable=False)
+  name = db.Column(db.String(100), nullable=False)
+  email = db.Column(db.String(100), unique=True, nullable=False)
+  location = db.Column(db.String(100), nullable=False)
+  biography = db.Column(db.String(100))
+  photo = db.Column(db.String(50), nullable=False)
+  date_joined = db.Column(db.DateTime, nullable=False, default=datetime.now)
+  
+  def __init__(self, username, password, name, email, location, biography, photo, date_joined):
+    self.username = username
+    self. password = generate_password_hash(password, method='pbkdf2:sha256')
+    self.name = name
+    self. email = email
+    self.location = location
+    self.biography = biography
+    self.photo = photo
+    self.date_joined = datetime.now()
+  
+
+     
+  def is_authenticated(self):
+    return True
+
+  def is_active(self):
+    return True
+
+  def is_anonymous(self):
+    return False
+  
+  def get_id(self):
+    try:
+      return unicode(self.id)  # python 2 support
+    except NameError:
+      return str(self.id)  # python 3 support
+    
   def __repr__(self):
-    return '<Car: %r %r %r %r %r>' % (self.colour, self.make, self.model, self.car_type, self.price)
-   
-#def is_authenticated(self):
-#  return True
-
-#def is_active(self):
-#  return True
-
-#def is_anonymous(self):
-#  return False
-  
-  
+    return '<User %r>' % (self.username)
   
